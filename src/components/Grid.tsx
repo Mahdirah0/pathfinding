c043b194a3b3fbba4581f2c073ff19f8782966a4
Mahdirah0 <rahmanmahdi15@gmail.com>
2022-10-08 20:47:24 +0100

    changes

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,23 +1,33 @@
 import { useEffect, useState } from 'react';
-import VisualizeBoard from './VisualizeGrid';
+import VisualizeBoard from './VisualizeBoard';
 import './Grid.css';
-// import { Board } from '../Board';
+import {
+  INTIAL_END_ROW,
+  INTIAL_END_COL,
+  INTIAL_START_COL,
+  INTIAL_START_ROW,
+} from '../constant';
 
 const Board = () => {
-  const initStartRow = 5;
-  const initStartCol = 3;
-  const initEndRow = 5;
-  const initEndCol = 17;
   const [grid, setGrid] = useState<string[][]>([]);
   const [nodeClicked, setNodeClicked] = useState({
     x: 0,
     y: 0,
     node: '',
   });
+  const [dragging, setDragging] = useState({
+    start: false,
+    end: false,
+  });
 
-  // const startNodePos: number[] = [];
-  const [endNodePos, setEndNodePos] = useState<number[]>();
-  const [startNodePos, setStartNodePos] = useState<number[]>();
+  const [startNodePos, setStartNodePos] = useState({
+    x: INTIAL_START_COL,
+    y: INTIAL_START_ROW,
+  });
+  const [endNodePos, setEndNodePos] = useState({
+    x: INTIAL_END_COL,
+    y: INTIAL_END_ROW,
+  });
 
   useEffect(() => {
     const initialGrid: string[][] = createBoard(11, 21);
@@ -26,8 +36,8 @@ const Board = () => {
 
   const createBoard = (row: number, col: number) => {
     const initialGrid = Array.from(Array(row), () => new Array(col).fill(''));
-    initialGrid[initStartRow][initStartCol] = 'S';
-    initialGrid[initEndRow][initEndCol] = 'E';
+    initialGrid[INTIAL_START_ROW][INTIAL_START_COL] = 'S';
+    initialGrid[INTIAL_END_ROW][INTIAL_END_COL] = 'E';
     return initialGrid;
   };
 
@@ -36,36 +46,31 @@ const Board = () => {
     setGrid(arr);
   };
 
-  const handleMouseDown = (x: number, y: number, event: any) => {
-    console.log(event);
-    console.log(`row: ${y}, col: ${x}`);
-    let copyArray = [...grid];
-    const node = copyArray[y][x];
-
-    if (node === 'W' || node === 'S' || node === 'E') {
-      copyArray[y][x] = '';
-      setGrid(copyArray);
-    } else if (node === '') {
-      copyArray[y][x] = 'W';
-      setGrid(copyArray);
+  const onMouseDown = (x: number, y: number, event: any) => {
+    // draggiing to be true when
+    // (green node) x != INITIAL_START_COL, y != INTIAL_START_ROW
+    // (red node) x != INITIAL_END_COL, y != INTIAL_START_ROW
+    // click
+    // end click
+    if (x === startNodePos.x && y === startNodePos.y) {
+      setDragging({ start: true, end: false });
+    } else if (x === endNodePos.x && y === endNodePos.y) {
+      setDragging({ start: false, end: true });
     }
-
-    setNodeClicked({ node, x, y });
   };
 
-  const handleMouseUp = (x: number, y: number, event: any) => {
-    console.log(nodeClicked);
-    console.log(y, x);
-    const { node } = nodeClicked;
-    let copyArray = [...grid];
-    copyArray[y][x] = node;
-    setGrid(copyArray);
+  const onMouseUp = (x: number, y: number, event: any) => {
+    setDragging({ start: false, end: false });
+  };
 
-    // let copyArray = [...grid];
-    // copyArray[y][x] = 'S';
+  const onMouseMove = (x: number, y: number, event: any) => {
+    console.log(dragging.start, dragging.end);
+    if (dragging.start) {
+    }
+  };
 
-    // setGrid(copyArray);
-    // console.log(grid);
+  const onClick = (x: number, y: number, event: any) => {
+    // console.log(dragging);
   };
 
   return (
@@ -73,8 +78,10 @@ const Board = () => {
       <div className='board'>
         <VisualizeBoard
           board={grid}
-          onMouseDown={handleMouseDown}
-          onMouseUp={handleMouseUp}
+          onMouseDown={onMouseDown}
+          onMouseUp={onMouseUp}
+          onClick={onClick}
+          onMouseMove={onMouseMove}
         />
       </div>
     </>
--- src/components/VisualizeBoard.tsx
+++ src/components/VisualizeBoard.tsx
@@ -1,28 +1,38 @@
 import { FC } from 'react';
-import './VisualizeGrid.css';
+import './VisualizeBoard.css';
 
 interface IProps {
   board: string[][];
   // handleClick: (x: number, y: number, event: any) => void;
   onMouseDown: (x: number, y: number, event: any) => void;
   onMouseUp: (x: number, y: number, event: any) => void;
+  onClick: (x: number, y: number, event: any) => void;
+  onMouseMove: (x: number, y: number, event: any) => void;
 }
 
-const VisualizeBoard: FC<IProps> = ({ board, onMouseDown, onMouseUp }) => {
+const VisualizeBoard: FC<IProps> = ({
+  board,
+  onMouseDown,
+  onMouseUp,
+  onClick,
+  onMouseMove,
+}) => {
   return (
     <>
       {board.map((item: any, rowIndex: number) => (
         <div key={rowIndex} className='rows' row-number={rowIndex}>
-          {item.map((value: any, colIndex: number) => (
+          {item.map((node: any, colIndex: number) => (
             <div
               key={colIndex}
               className='cols'
               cell-coordinates={`${rowIndex},${colIndex}`}
-              start-node={value === 'S' ? 'true' : undefined}
-              end-node={value === 'E' ? 'true' : undefined}
-              wall-node={value === 'W' ? 'true' : undefined}
+              start-node={node === 'S' ? 'true' : undefined}
+              end-node={node === 'E' ? 'true' : undefined}
+              wall-node={node === 'W' ? 'true' : undefined}
               onMouseDown={(event) => onMouseDown(colIndex, rowIndex, event)}
               onMouseUp={(event) => onMouseUp(colIndex, rowIndex, event)}
+              onClick={(event) => onClick(colIndex, rowIndex, event)}
+              onMouseMove={(event) => onMouseMove(colIndex, rowIndex, event)}
             ></div>
           ))}
         </div>
